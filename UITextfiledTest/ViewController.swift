9b677ea647796cfce8fd554122c7439e452760d3
Jenaral <Jenaral@JenaraleMacBook.lan>
2016-07-09 15:14:52 +0800

    rePush Code

--- UITextfiledTest/ViewController.swift
+++ UITextfiledTest/ViewController.swift
@@ -7,12 +7,37 @@
 //
 
 import UIKit
+import ReactiveCocoa
 
 class ViewController: UIViewController {
 
+    @IBOutlet var textFeildOne: UITextField!
+    
+    @IBOutlet var textFieldTwo: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        bindTextField()
+    }
+    
+    func bindTextField() {
+//        let textOne = textFeildOne.rac_textSignal().toSignalProducer().map { text in text as! String
+//        }
+        
+        textFeildOne.rac_textSignal().toSignalProducer().startWithNext { [unowned self](text) in
+            if let str = text as? String {
+                self.textFieldTwo.text = str
+            }
+        }
+        
+        textFieldTwo.rac_textSignal().toSignalProducer().startWithNext { [unowned self](text) in
+            if let str = text as? String {
+                self.textFeildOne.text = str
+            }
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
